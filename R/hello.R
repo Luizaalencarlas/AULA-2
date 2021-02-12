0df79caeea16bb0086702311fe5c84f513446eda
Luiza Alencar Sales <l00540197@china.huawei.com>
2021-02-11 22:43:26 -0300

    first commit

---
+++ R/hello.R
@@ -0,0 +1,40 @@
+#' function to sum(titulo)
+#'
+#' This function is good to sum.
+#'
+#'@param a A number.
+#'@param b It is another number.
+#'
+#'@examples
+#'somar(2, 3)
+#'5
+#'
+#' @export
+somar <- function(a,b) {
+  sum(a,b)
+}
+#' function to subtract
+#'
+#' This function is good to subtract.
+#'
+#'@param a A number.
+#'@param b It is another number.
+#'
+#'@examples
+#'subtract(2, 3)
+#'-1
+#'
+#' @export
+subtract <- function(a,b){
+  a - b
+}
+
+
+
+
+
+
+
+
+
+
